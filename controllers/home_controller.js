d2ed3ee123ca021cd9ab187157c269ddc4648cde
Himadri Das <codingmaster40@gmail.com>
2023-06-04 00:57:14 +0530

    created a view for home_controller.home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up a for Codeial!</h1>');
+    // return res.end('<h1>Express is up a for Codeial!</h1>');
+    return res.render('home', {
+        title : "Home"
+    })
+    //since app.set('views', './views') => so 'home' targets '../views/home.ejs' and we pass an object with key and values
 }
