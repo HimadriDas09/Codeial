6346bcd051ac7f4081ebdb83aeeac66cdb706ac1
Himadri Das <codingmaster40@gmail.com>
2023-07-14 03:41:33 +0530

    first layouts : installing express-ejs-layouts, require it in entry, only keeping variable part in ejs returned from view

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
+
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts); /* mention that we're using express layouts before routes => bcz it then renders (layout part + res.render()) part together and display it on the browser */
 
 //use express router
 app.use('/', require('./routes'));
