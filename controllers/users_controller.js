6bf67f9889170db916ce967db2c6e6e9b569fb64
Himadri Das <codingmaster40@gmail.com>
2023-07-14 02:55:47 +0530

    creating our first partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 //userController.profile => contains below action
 module.exports.profile = function(req, res){
-    return res.end('<h1>Users Profile</h1>');
+    return res.render('user_profile', {
+        title : "Home"
+    });
 }
